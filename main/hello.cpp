7f3ff80b4f23cba4efa738acf53c8bdcb21c0c52
ANEESH K B <kbaneesh2002@gmail.com>
2024-03-08 11:33:05 +0530

    chh

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,7 +6,7 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run\n";
-  cout<<"Hello i am in PES1UG21CS080\n";
+  cout<<"Hello i am inside PES1UG21CS080\n";
   
   return 0;
 }
